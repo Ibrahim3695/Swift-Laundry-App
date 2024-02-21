dc823c636f2bd9222966677efcfd5746328cf1a3
olawale ibrahim <ibrahimolawale3695@gmail.com>
2024-02-21 16:50:19 +0100

    dd

--- src/howWeWorkComponent/HowWeWork.jsx
+++ src/howWeWorkComponent/HowWeWork.jsx
@@ -4,8 +4,15 @@ import "./HowWeWork.css"
 const HowWeWork = () => {
   return (
     <div className='howWeWorkMainDiv'>
-      <section className='  '></section>
-      <section></section>
+      <section className='weWorkTopDiv'>
+        <p>The process</p>
+        <samp>How we work</samp>
+        </section>
+      <section className='weWorkBottomDiv'>
+        <article className='pickUpDiv'></article>
+        <article className='washDiv'></article>
+        <article className='deliverDiv'></article>
+      </section>
     </div>
   )
 }
